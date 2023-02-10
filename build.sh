3f1fb6d56361b01d9dbc6538f928d2ecbd020328
旭日生辉 <676824363@qq.com>
2023-02-10 17:14:51 +0800

    添加更多验证文件

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 git fetch --unshallow
 # 构建
 npm run docs:build
-# 复制360搜索站点验证文件
-mv public/991d9149430c83a7c3ffd556d806d0ee.txt \
-    src/.vuepress/dist/991d9149430c83a7c3ffd556d806d0ee.txt
+# 复制第三方文件,这个文件夹用于放置与网站本身工作无关,但是需要被部署的文件
+# 例如搜索引擎的所有权验证文件
+mv -u public/* src/.vuepress/dist/*
