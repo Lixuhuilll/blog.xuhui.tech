96745f1de88d10f4b4f45ac5939765c18fad034a
lixuhuilll <676824363@qq.com>
2023-12-28 20:59:40 +0800

    测试 Cloudflare Pages V2 构建系统

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # 这会导致git相关的插件无法正常工作
 git fetch --unshallow
 # 构建
-npm run docs:build
+pnpm run docs:build
 # 复制第三方文件,这个文件夹用于放置与网站本身工作无关,但是需要被部署的文件
 # 例如搜索引擎的所有权验证文件
 mv -u public/* src/.vuepress/dist/
